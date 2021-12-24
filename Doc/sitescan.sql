e03a14962526ba615aae29c31c7c1fcbcd53881f
abbasli-g <a.gharakhanlou@gmail.com>
2021-12-24 16:55:21 +0330

    14001003--mssql sitescan script

--- Doc/sitescan.sql
+++ Doc/sitescan.sql
@@ -1,3 +1,5 @@
+--Mysql
+
 CREATE TABLE sitescan (
 	id INT auto_increment NOT NULL,
 	sitecontent varchar(3000) NULL,
@@ -8,3 +10,15 @@ ENGINE=InnoDB
 DEFAULT CHARSET=utf8
 COLLATE=utf8_general_ci;
 
+
+--Mssql
+
+CREATE TABLE sitescan(
+[id] [int] IDENTITY(1,1) NOT FOR REPLICATION NOT NULL,
+[sitecontent] [varchar](3000) NULL,
+[regDateTime] [datetime2](7) NULL,
+CONSTRAINT [PK_sitescan] PRIMARY KEY CLUSTERED
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
